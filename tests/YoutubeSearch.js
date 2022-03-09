06352c7f782aaab516d2d8480a0a3f49ff945007
erenhizm <99208090+erenhizm@users.noreply.github.com>
2022-03-09 17:50:29 +0300

    Update YoutubeSearch.js

--- tests/YoutubeSearch.js
+++ tests/YoutubeSearch.js
@@ -2,5 +2,9 @@ const Novader = require("novader")
 const scs = new Novader.Search("YOUTUBE_APİ_KEY");  
 let SearchVideo = await scs.SearchVideo("PAPARAZZİ")
 console.log(SearchVideo)
+
 let getVideo = await scs.VideoGet("https://www.youtube.com/watch?v=zrcOsxFxqG0")
 console.log(getVideo)
+
+let getPlayList = await scs.getPlaylist("https://www.youtube.com/watch?v=82PsUyCKuUc&list=PLblKrj0aH7t6Me4jzOxNSIxC2Ki1qZmyG")
+console.log(getPlayList)
