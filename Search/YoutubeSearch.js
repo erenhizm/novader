8b3054af9fd38521f8b18491741c064e67f62b56
erenhizm <99208090+erenhizm@users.noreply.github.com>
2022-03-10 19:06:26 +0300

    Create YoutubeSearch.js

---
+++ Search/YoutubeSearch.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js")
+const client = new Discord.Client()
+const Novader = require("novader")
+const scs = new Novader.Search("YOUTUBE_APİ_KEY");
+module.exports.run = async (message, client, args) => {
+  if(!args.join('')){return message.reply("Please Enter Youtube Video")}
+const song = await scs.SearchVideo(args.join(' '))
+message.channel.send(`**Song Name**: ${song.title}\n**Song publication** : ${song.publication} \n**Song Channel Name**:${song.channel.name}`)
+};
+module.exports.help = {
+    name:"test",
+    aliases:["novader"]
+}
