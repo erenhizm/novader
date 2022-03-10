7dc1b82b4518a26be14d937429f47bc10403d6db
erenhizm <99208090+erenhizm@users.noreply.github.com>
2022-03-10 19:09:02 +0300

    Update YoutubeSearch.js

--- Search/YoutubeSearch.js
+++ Search/YoutubeSearch.js
@@ -5,6 +5,7 @@ const scs = new Novader.Search("YOUTUBE_APİ_KEY");
 module.exports.run = async (message, client, args) => {
   if(!args.join('')){return message.reply("Please Enter Youtube Video")}
 const song = await scs.SearchVideo(args.join(' '))
+if(!song){return message.reply("No Video")}
 message.channel.send(`**Song Name**: ${song.title}\n**Song publication** : ${song.publication} \n**Song Channel Name**:${song.channel.name}`)
 };
 module.exports.help = {
