a88a8dc61f3094a5ad64b8e06320ee7e85d17b62
erenhizm <99208090+erenhizm@users.noreply.github.com>
2022-03-10 19:11:20 +0300

    Update getPlaylist.js
    YOUR SET

--- Search/getPlaylist.js
+++ Search/getPlaylist.js
@@ -1,1 +1,15 @@
-a
+
+const Discord = require("discord.js")
+const client = new Discord.Client()
+const Novader = require("novader")
+const scs = new Novader.Search("YOUTUBE_APİ_KEY");
+module.exports.run = async (message, client, args) => {
+  if(!args.join('')){return message.reply("Please Enter Youtube Playlist")}
+const song = await scs.getPlaylist(args[0])
+if(!song){return message.reply("No Video")}
+console.log(song)
+};
+module.exports.help = {
+    name:"yt-playlist",
+    aliases:["yt-playlist-info","yts-playlist","search-playlist-youtube"]
+}
