bb47a3b5eaff0268071ad6de17e3776a65e0dc0f
erenhizm <99208090+erenhizm@users.noreply.github.com>
2022-06-18 16:23:06 +0300

    CommandHandler

--- CommandHandler/commands/pıng.js
+++
@@ -1,7 +0,0 @@
-module.exports.run = async (message, client, args) => {
-message.channel.send(client.ws.ping)
-};
-module.exports.help = {
-    name:"ping",
-    aliases:["dley","bot-ws","bot-ping"]
-}
